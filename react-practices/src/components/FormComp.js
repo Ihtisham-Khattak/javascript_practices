de970d88c74eed502ca9d83174c5fd407759fbf1
Ihtisham-Khattak <ikpashteen1@gmail.com>
2023-09-08 11:01:54 +0500

    changes

--- react-practices/src/components/FormComp.js
+++ react-practices/src/components/FormComp.js
@@ -1,16 +1,36 @@
-import React, {useState} from "react";
+import React, { useState, useEffect } from "react";
 
 const FormComp = () => {
-  const [userName, setUserName] = useState({ name: "", isCorrect: true });
+
+
+  const [userName, setUserName] = useState({ nameValue: "", isCorrect: true });
+
+  const addBTN = () => {
+    const inputValue = document.querySelector("input[name='userName']").value;
+    setUserName((prev) => ({ ...prev, nameValue:inputValue }));
+  };
+  
+  const correctBTN = () => {
+    setUserName((prev) => ({ ...prev, isCorrect: false }));
+  };
+
+  useEffect(() => {
+    console.log ("useeffect Call")
+  }, [])
+
   return (
     <div>
       <h2>Form Control</h2>
-      <input name="userName" onChange={((e) => (e.target.value))} />
+      <input name="userName"  onChange={(e) => e.target.value} />
       <br />
-      <button type="button" onClick={() => addBTN()}>Add Name</button>
-      <button type="button" onClick={correctBTN}>Add Correct</button>
+      <button type="button" onClick={() => addBTN()}>
+        Add Name
+      </button>
+      <button type="button" onClick={() => correctBTN()}>
+        Add Correct
+      </button>
       <br />
-      {`${userName.name} is like ${userName.isCorrect.toString()}`}
+      {`${userName.nameValue} is like ${userName.isCorrect.toString()}`}
     </div>
   );
 };
